98633f5f5d67bef48c31a56a20709b96c9720463
josephemswiler <josephemswiler@gmail.com>
2018-08-27 16:19:56 -0500

    init modal

--- src/components/elements/Modal.js
+++ src/components/elements/Modal.js
@@ -28,12 +28,12 @@ export default class Modal extends Component {
           toggle={this.toggle}
           className={this.props.className}
         >
-          <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
+          <ModalHeader toggle={this.toggle}>Private Repo / WIP</ModalHeader>
           <ModalBody>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            This project is currently under development and the repository is private. For access to the current working prototype and/or the repository, please email me at josephemswiler@gmail.com.
           </ModalBody>
           <ModalFooter>
-            <Button color='primary' onClick={this.toggle}>Do Something</Button>
+            <Button color='primary' onClick={this.toggle}>Awesome</Button>
             {' '}
             <Button color='secondary' onClick={this.toggle}>Cancel</Button>
           </ModalFooter>
