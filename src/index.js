52d18a6f130f2b26a1dc2eb78d80e37aca545c11
josephemswiler <josephemswiler@gmail.com>
2018-08-27 16:16:38 -0500

    gh-pages deploy updates

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import 'bootswatch/dist/materia/bootstrap.css'
-import { BrowserRouter as Router } from 'react-router-dom'
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(
-<Router><App /></Router>
-, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
